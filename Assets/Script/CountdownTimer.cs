fe1177f11a2f51abe7e9c2d4ed95fd286a1a2337
JMVD99 <56115546+JMVD99@users.noreply.github.com>
2020-01-18 07:52:56 +0100

    07:52

---
+++ Assets/Script/CountdownTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float currentTime = 0f;
+    float startingTime = 120f;
+
+    public int index;
+    public string levelName;
+
+    [SerializeField] Text CountDownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        CountDownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+
+            //laad level met build uit de index 
+            SceneManager.LoadScene(index);
+            
+            //laad level met scene name
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
